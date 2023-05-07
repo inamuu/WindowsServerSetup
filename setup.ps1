bdfc09ffacdfa4abfae38009585a89c58604871e
inamuu <8310973+inamuu@users.noreply.github.com>
2023-05-07 17:10:22 +0900

    add if branch when false behavior

--- setup.ps1
+++ setup.ps1
@@ -11,13 +11,16 @@ $Apache="https://www.apachelounge.com/download/VS17/binaries/httpd-2.4.57-win64-
 
 ### Create Setup Directory
 
-mkdir $Desktop\ServerSetup
+if (-not(TEST-Path $SetupDir)) {
+  mkdir $Desktop\ServerSetup
+}
 
 ### Install Sakura Editor
 
-"Install Sakura Editor" | Out-GridView 
+if (-not(TEST-Path $SetupDir\sakura)) {
+  mkdir $SetupDir\sakura
+}
 
-mkdir $SetupDir\sakura 
 wget $SakuraEditor -O $SetupDir\sakura\sakura.zip
 Expand-Archive -Path $SetupDir\sakura\sakura.zip -DestinationPath $SetupDir\sakura -Force
 mv $SetupDir\sakura\sakura*.exe $SetupDir\sakura\sakura_installer.exe
@@ -25,10 +28,11 @@ Start-Process -FilePath $SetupDir\sakura\sakura_installer.exe -ArgumentList "/S"
 
 ### Install Apache
 
-"Install Apache" | Out-GridView 
+if (-not(TEST-Path $SetupDir\apache)) {
+  mkdir $SetupDir\apache
+}
 
-mkdir $SetupDir\apache
-curl -L -o $SetupDir\apache\apache.zip $Apache 
-xpand-Archive -Path $SetupDir\apache\apache.zip -DestinationPath $SetupDir\apache -Force
+curl -L -o $SetupDir\apache\apache.zip $Apache
+Expand-Archive -Path $SetupDir\apache\apache.zip -DestinationPath $SetupDir\apache -Force
 cp -rp $SetupDir\apache\Apache24 "C:\"
 
