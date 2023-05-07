4653f6e45f614d7641a99fb275c52855da2e9aba
inamuu <8310973+inamuu@users.noreply.github.com>
2023-05-07 16:00:53 +0900

    Install apache

--- setup.ps1
+++ setup.ps1
@@ -1,19 +1,19 @@
 # WindowsServerSetup
 
-
-### 定数
+### Variables
 
 $Desktop="$HOME\Desktop"
 $SetupDir="$HOME\Desktop\ServerSetup"
 
-### ソフトウェアのパス
+### Download Path
 $SakuraEditor="https://github.com/sakura-editor/sakura/releases/download/v2.4.2/sakura-tag-v2.4.2-build4203-a3e63915b-Win32-Release-Installer.zip"
+$Apache="https://www.apachelounge.com/download/VS17/binaries/httpd-2.4.57-win64-VS17.zip"
 
-### 作業フォルダ作成
+### Create Setup Directory
 
 mkdir $Desktop\ServerSetup
 
-### さくらエディタのインストール
+### Install Sakura Editor
 
 "Install Sakura Editor" | Out-GridView 
 
@@ -22,4 +22,13 @@ wget $SakuraEditor -O $SetupDir\sakura\sakura.zip
 Expand-Archive -Path $SetupDir\sakura\sakura.zip -DestinationPath $SetupDir\sakura -Force
 mv $SetupDir\sakura\sakura*.exe $SetupDir\sakura\sakura_installer.exe
 Start-Process -FilePath $SetupDir\sakura\sakura_installer.exe -ArgumentList "/S" -Wait
+
+### Install Apache
+
+"Install Apache" | Out-GridView 
+
+mkdir $SetupDir\apache
+curl -L -o $SetupDir\apache\apache.zip $Apache 
+xpand-Archive -Path $SetupDir\apache\apache.zip -DestinationPath $SetupDir\apache -Force
+cp -rp $SetupDir\apache\Apache24 "C:\"
 
